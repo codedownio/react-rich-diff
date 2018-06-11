0b4efe42e9a22437854e8ca8662662e0a077ca71
Tom McLaughlin <pyro777@gmail.com>
2018-06-11 05:13:20 -0700

    Export necessary MarkupIt call from index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const State = require('./diffing/State');
 const RichDiff = require('./components/RichDiff');
 
+const MarkupIt = require("markup-it");
+const markdown = require("markup-it/lib/markdown");
+
 RichDiff.State = State;
+
+RichDiff.getMarkupItState = function() {
+    return MarkupIt.State.create(markdown);
+};
 
 module.exports = RichDiff;
